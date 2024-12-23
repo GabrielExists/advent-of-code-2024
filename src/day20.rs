d726a236552c62c4d3e39b9324a6a81c184ba1be
Gabriel <gabriel.exists.concurrently@gmail.com>
2024-12-23 02:26:47 +0100

    Solve day 20

--- src/day20.rs
+++ src/day20.rs
@@ -5,7 +5,7 @@ use crate::app::{DayOutput, Diagnostic, Tab};
 use crate::grid::{Coord, Grid};
 
 type Key = Coord;
-type TileData = (u64, HashMap<Key, u64>);
+type TileData = (u64, HashSet<Key>);
 type CandidateMap = HashMap<Key, TileData>;
 
 const LIMIT: u64 = 100;
@@ -39,7 +39,7 @@ pub fn puzzle(input: &str) -> DayOutput {
     let mut tabs = vec![];
     let mut errors: Vec<String> = vec![];
     let mut silver = 0;
-    let mut gold = String::new();
+    let mut gold = 0;
     tabs.push(Tab {
         title: "Input".to_string(),
         strings: vec![],
@@ -47,35 +47,22 @@ pub fn puzzle(input: &str) -> DayOutput {
     });
     if let (Some(start), Some(end)) = (start, end) {
         let explored = pathfind(&input_grid, &mut tabs, start, end);
-        let (path_tiles, default_total_steps) = follow_path(&explored, end, true);
-        add_tab_path(&mut tabs, &input_grid, path_tiles, default_total_steps, format!("Default"));
+        let path_tiles = follow_path(&explored, end, true);
+        let default_total_steps = (path_tiles.len() - 1) as u64;
+        add_tab_path(&mut tabs, &input_grid, path_tiles, format!("Default"));
 
-        let mut cheats: Vec<((Coord, Coord), u64)> = Vec::new();
+        let mut cheats_silver: Vec<((Coord, Coord), u64)> = Vec::new();
+        let mut cheats_gold: Vec<((Coord, Coord), u64)> = Vec::new();
         for cheat_from in explored.keys() {
             for cheat_to in explored.keys() {
                 let steps = steps_away(cheat_from, cheat_to);
-                if steps <= 2 && cheat_to != cheat_from {
-                    // if let Some((end_key, _cost)) = explored.get(&end).map(|tile_data| (end, tile_data.0)) {
-                    //     let path_tiles: HashSet<Coord> = follow_path_with_cheat(&explored, end_key, true);
-                    // }
-                    // add_tab_path(&input_grid, &explored, end, &mut tabs, format!("F{} {}", cheat_from, cheat_to));
-                    let mut explored = explored.clone();
-                    if let Entry::Occupied(data_to) = explored.entry(*cheat_to) {
-                        let cost_to = data_to.get().0 + steps as u64;
-                        if let Entry::Occupied(mut data_from) = explored.entry(*cheat_from) {
-                            let data_from = data_from.get_mut();
-                            combine_tile_data(data_from, &(cost_to, HashMap::from([(*cheat_to, steps as u64)])));
-                            let (path_tiles, total_steps) = follow_path(&explored, end, true);
-                            // add_tab_path(&mut tabs, &input_grid, path_tiles, total_steps, format!("F{} {}", cheat_from, cheat_to));
-                            cheats.push(((*cheat_from, *cheat_to), total_steps));
-                        }
-                    }
-                }
+                try_cheat(&explored, &mut cheats_silver, cheat_from, cheat_to, steps, 2);
+                try_cheat(&explored, &mut cheats_gold, cheat_from, cheat_to, steps, 20);
             }
         }
         tabs.push(Tab {
             title: "Cheats".to_string(),
-            strings: cheats.iter().filter_map(|cheat| {
+            strings: cheats_silver.iter().filter_map(|cheat| {
                 if cheat.1 != default_total_steps {
                     Some(format!("{:?}", cheat))
                 } else {
@@ -84,19 +71,34 @@ pub fn puzzle(input: &str) -> DayOutput {
             }).collect(),
             grid: vec![],
         });
-        let mut saved_to_num = HashMap::new();
-        for (_key, total_cost) in cheats.iter() {
-            let cost_diff = default_total_steps - total_cost;
-            let mut num = saved_to_num.entry(cost_diff).or_insert(0);
+        let mut saved_to_num_silver = HashMap::new();
+        for (_key, delta) in cheats_silver.iter() {
+            let mut num = saved_to_num_silver.entry(delta).or_insert(0);
             *num += 1;
-            if cost_diff >= LIMIT {
+            if *delta >= LIMIT {
                 silver += 1;
             }
         }
 
         tabs.push(Tab {
             title: "Cheat summary".to_string(),
-            strings: saved_to_num.iter().map(|(saved, num)| {
+            strings: saved_to_num_silver.iter().map(|(saved, num)| {
+                format!("saved: {}, num: {}", saved, num)
+            }).collect(),
+            grid: vec![],
+        });
+        let mut saved_to_num_gold = HashMap::new();
+        for (_key, delta) in cheats_gold.iter() {
+            let mut num = saved_to_num_gold.entry(delta).or_insert(0);
+            *num += 1;
+            if *delta >= LIMIT {
+                gold += 1;
+            }
+        }
+
+        tabs.push(Tab {
+            title: "Cheat summary gold".to_string(),
+            strings: saved_to_num_gold.iter().map(|(saved, num)| {
                 format!("saved: {}, num: {}", saved, num)
             }).collect(),
             grid: vec![],
@@ -127,11 +129,26 @@ pub fn puzzle(input: &str) -> DayOutput {
     }
 }
 
+fn try_cheat(explored: &CandidateMap, cheats: &mut Vec<((Coord, Coord), u64)>, cheat_from: &Key, cheat_to: &Key, steps: i32, max_steps: i32) {
+    if steps <= max_steps && cheat_to != cheat_from {
+        if let (Some(data_from), Some(data_to)) = (explored.get(cheat_from), explored.get(cheat_to)) {
+            let cost_from = data_from.0;
+            let cost_to = data_to.0;
+            if cost_to > cost_from {
+                if (cost_to - cost_from) > steps as u64 {
+                    let delta = (cost_to - cost_from) - steps as u64;
+                    cheats.push(((*cheat_from, *cheat_to), delta));
+                }
+            }
+        }
+    }
+}
+
 fn steps_away(start: &Coord, end: &Coord) -> i32 {
     i32::abs(start.0.0 - end.0.0) + i32::abs(start.0.1 - end.0.1)
 }
 
-fn add_tab_path(tabs: &mut Vec<Tab>, grid: &Grid<Tile>, path_tiles: HashSet<Coord>, total_cost: u64, title: String) {
+fn add_tab_path(tabs: &mut Vec<Tab>, grid: &Grid<Tile>, path_tiles: HashSet<Coord>, title: String) {
     let mut grid = grid.clone();
     // Apply the path tiles
     for pos in path_tiles.iter() {
@@ -141,7 +158,7 @@ fn add_tab_path(tabs: &mut Vec<Tab>, grid: &Grid<Tile>, path_tiles: HashSet<Coor
     }
     tabs.push(Tab {
         title,
-        strings: vec![format!("{} nanos", total_cost)],
+        strings: vec![format!("{} nanos", path_tiles.len() - 1)],
         grid: grid.to_tab_grid(),
     });
 }
@@ -164,7 +181,7 @@ fn pathfind(input_grid: &Grid<Tile>, mut tabs: &mut Vec<Tab>, start: Coord, end:
             for dir in Coord::get_orthagonal_dirs() {
                 let new_position = source.add(&dir);
                 let new_cost = frontier_cost + 1;
-                let new_came_from = HashMap::from([(key, 1)]);
+                let new_came_from = HashSet::from([key]);
                 let (exists, exists_but_expensive) = if let Some((explored_cost, explored_came_from)) = explored.get_mut(&new_position) {
                     if *explored_cost == new_cost {
                         explored_came_from.extend(new_came_from.iter());
@@ -195,7 +212,7 @@ fn pathfind(input_grid: &Grid<Tile>, mut tabs: &mut Vec<Tab>, start: Coord, end:
 fn create_frontier(grid: &Grid<Tile>, start: Coord) -> CandidateMap {
     let start_tile = grid.get(start);
     if let Some(Tile::Empty) = start_tile {
-        HashMap::from_iter([(start, (0, HashMap::new()))].into_iter())
+        HashMap::from_iter([(start, (0, HashSet::new()))].into_iter())
     } else {
         HashMap::new()
     }
@@ -224,18 +241,15 @@ fn combine_tile_data(first: &mut TileData, second: &TileData) {
     }
 }
 
-fn follow_path(map: &CandidateMap, end: Key, only_shortest: bool) -> (HashSet<Coord>, u64) {
+fn follow_path(map: &CandidateMap, end: Key, only_shortest: bool) -> HashSet<Coord> {
     if let Some((end_key, _cost)) = map.get(&end).map(|tile_data| (end, tile_data.0)) {
-        let mut remaining_tiles = HashMap::from([(end_key, 0)]);
+        let mut remaining_tiles = HashSet::from([end_key]);
         let mut path_tiles = HashSet::<Coord>::new();
-        let mut total_steps = 0;
         while !remaining_tiles.is_empty() {
-            if let Some((next_key, steps)) = remaining_tiles.iter().next() {
+            if let Some(next_key) = remaining_tiles.iter().next() {
                 let next_key = next_key.clone();
-                let steps = steps.clone();
                 remaining_tiles.remove(&next_key);
                 path_tiles.insert(next_key);
-                total_steps += steps;
                 if let Some((_cost, came_from)) = map.get(&next_key) {
                     if only_shortest {
                         remaining_tiles.extend(came_from.iter().take(1));
@@ -245,9 +259,9 @@ fn follow_path(map: &CandidateMap, end: Key, only_shortest: bool) -> (HashSet<Co
                 }
             }
         }
-        (path_tiles, total_steps)
+        path_tiles
     } else {
-        (HashSet::new(), 0)
+        HashSet::new()
     }
 }
 
