91a63fdfc6292bb6431816066972e80c9e810454
Gabriel <gabriel.exists.concurrently@gmail.com>
2025-03-31 14:14:17 +0200

    Create off topic iterator for skipping specific indices

--- src/day2.rs
+++ src/day2.rs
@@ -2,6 +2,57 @@ use regex::Regex;
 use Direction::*;
 use crate::app::{DayOutput, Diagnostic};
 
+trait SkipSpec<S>: Iterator + Sized {
+    fn skip_specific(self, skip_index: S) -> SkipSpecific<Self, S>;
+}
+
+struct SkipSpecific<T, S> {
+    iter: T,
+    skip_indices: S,
+    current_index: usize,
+}
+
+impl<T, S> SkipSpec<S> for T
+where T: Iterator, S: SkipIndices {
+    fn skip_specific(self, skip_indices: S) -> SkipSpecific<Self, S> {
+        SkipSpecific {
+            iter: self,
+            skip_indices,
+            current_index: 0,
+        }
+    }
+}
+
+impl<T, S> Iterator for SkipSpecific<T, S>
+where T: Iterator, S: SkipIndices {
+    type Item = T::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+        while self.skip_indices.contains_index(self.current_index) {
+            self.iter.next()?;
+            self.current_index += 1;
+        }
+        self.current_index += 1;
+        self.iter.next()
+    }
+}
+
+trait SkipIndices {
+    fn contains_index(&self, index: usize) -> bool;
+}
+
+impl SkipIndices for usize {
+    fn contains_index(&self, index: usize) -> bool {
+        *self == index
+    }
+}
+impl SkipIndices for &[usize] {
+    fn contains_index(&self, index: usize) -> bool {
+        self.iter().any(|item| *item == index)
+    }
+}
+
 pub fn puzzle(input: &str) -> DayOutput {
     let re = Regex::new(r"([0-9]*) ").unwrap();
     let mut num_matches = 0;
@@ -33,7 +84,7 @@ pub fn puzzle(input: &str) -> DayOutput {
     let mut safe_reports_allowing_one = 0;
     let mut error_message = None;
     for (_index, report) in reports.into_iter().enumerate() {
-        match judge_safety(&report) {
+        match judge_safety(report.iter().copied()) {
             Ok(()) => {
                 safe_reports += 1;
                 safe_reports_allowing_one += 1;
@@ -45,7 +96,8 @@ pub fn puzzle(input: &str) -> DayOutput {
                 for i in 0..report.len() {
                     let mut current_report = report.clone();
                     current_report.remove(i);
-                    match judge_safety(&current_report) {
+                    // match judge_safety(current_report.iter().copied()) {
+                    match judge_safety(report.iter().skip_specific(&[i][..]).copied()) {
                         Ok(()) => {
                             safe_reports_allowing_one += 1;
                             break;
@@ -69,7 +121,8 @@ enum Direction {
     Decreasing,
 }
 
-fn judge_safety(report: &Vec<u64>) -> Result<(), String> {
+fn judge_safety<T: IntoIterator<Item=u64>>(report: T) -> Result<(), String>
+where T: IntoIterator<Item=u64>{
     let mut stored_previous = None;
     let mut direction = Undetermined;
     for (index, level) in report.into_iter().enumerate() {
